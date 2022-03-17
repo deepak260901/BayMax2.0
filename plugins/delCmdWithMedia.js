51fa802f3fe053aa093c50e502a72b5aa70a320b
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-18 02:54:25 +0530

    Update delCmdWithMedia.js

--- plugins/delCmdWithMedia.js
+++ plugins/delCmdWithMedia.js
@@ -1,7 +1,7 @@
 module.exports = Object.assign(async function handler(m, { text }) {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `Tidak ada hash`
+    if (!hash) throw `No hashes`
     let sticker = global.DATABASE._data.sticker
     if (sticker[hash] && sticker[hash].locked) throw 'You have no permission to delete this sticker command'
     delete sticker[hash]
