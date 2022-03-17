b698227979e1aeab1ad668b1182146bfa8dcfa31
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-18 01:16:35 +0530

    Update ceklimit.js

--- plugins/ceklimit.js
+++ plugins/ceklimit.js
@@ -2,7 +2,7 @@ let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    m.reply(`Tersisa ${global.DATABASE._data.users[who].limit} Limit Lagi`)
+    m.reply(`Tersisa ${global.DATABASE._data.users[who].limit} More Limits`)
 }
 handler.help = ['ceklimit']
 handler.tags = ['xp']
