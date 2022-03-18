a1e467d2c57bca50bbdfd65c8f741bfe6ae881c9
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-19 04:26:00 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,13 +1,9 @@
-let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  let contacts = []
-  for (let owner of Object.entries(global.Owner).filter(v => v[1].isCreator)) {
-    contacts.push(...[owner[0], (owner[1].name || conn.getName(owner[0]) || '')])
-  }
-  if (contacts.length < 3) return this.sendContact(m.chat, contacts[0], contacts[1], m)
-  this.sendContactArray(m.chat, contacts, m)
+function handler(m) {
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  this.sendContact(m.chat, '919609900020', 'Toshiro:)', m)
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner/creator']
 handler.tags = ['info']
 
 handler.command = /^(owner|creator)$/i
