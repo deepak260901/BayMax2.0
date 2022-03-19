ef0fbbb93a33ef22578eff4a678108991d791b2b
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-19 11:59:34 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -533,9 +533,9 @@ global.dfail = (type, m, conn) => {
     premium: 'This order is only for members _*Premium*_ !',
     group: 'This command can only be used in groups!',
     private: 'This command can only be used in Private Chat!',
-    admin: 'Perintah ini hanya untuk *Admin* grup!',
-    botAdmin: 'Jadikan bot sebagai *Admin* untuk menggunakan perintah ini!',
-    unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Manusia.16*'
+    admin: 'This command is only for *Admin* group!',
+    botAdmin: 'Make bot as *Admin* to use this command!',
+    unreg: 'Please register to use this feature by typing:\n\n*#register name.age*\n\nExample: *#register Toshiro.18*'
   }[type]
   if (msg) return m.reply(msg)
 }
