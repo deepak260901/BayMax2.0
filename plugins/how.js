6199bc60e6903a416b16e1cf9211fe5e249ae688
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-25 12:10:15 +0530

    Update how.js

--- plugins/how.js
+++ plugins/how.js
@@ -10,7 +10,7 @@ ${command} *${text}*
   } : {})
 }
 handler.help = ['gay', 'pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].map(v => 'how' + v + ' siapa?')
-handler.tags = ['kerang']
+handler.tags = ['How']
 handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy)/i
 handler.owner = false
 handler.mods = false
