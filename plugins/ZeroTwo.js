90656a36ec6932ccf1c583417835a00093db0577
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-27 03:26:50 +0530

    Update ZeroTwo.js

--- plugins/ZeroTwo.js
+++ plugins/ZeroTwo.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn ='src/sound/darling.mp3'
+let vn ='src/Sound/darling.mp3'
 conn.sendFile(m.chat, vn, 'darling.mp3', null, m, true, {
 type: 'audioMessage', // force no convert in ffmpeg
 ptt: true // true above doesn't work, because it's forced without converting ;v
