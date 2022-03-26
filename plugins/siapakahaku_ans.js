3a91d5a41111c976fee51ceab7f7629ad360b050
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-27 01:02:31 +0530

    Update siapakahaku_ans.js

--- plugins/siapakahaku_ans.js
+++ plugins/siapakahaku_ans.js
@@ -1,19 +1,19 @@
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*who/i.test(m.quoted.text)) return !0
-    conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
-    if (!(id in conn.siapakahaku)) return m.reply('Soal itu telah berakhir')
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*who/i.test(m.quoted.text)) return !0
+    conn.who am i = conn.who am i ? conn.who am i : {}
+    if (!(id in conn. who am I)) return m.reply('The matter has ended')
     if (m.quoted.id == conn.siapakahaku[id][0].id) {
-        let json = JSON.parse(JSON.stringify(conn.siapakahaku[id][1]))
+        let json = JSON.parse(JSON.stringify(conn.Who am I[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.result.jawaban.toLowerCase()) {
             global.DATABASE._data.users[m.sender].exp += conn.siapakahaku[id][2]
-            m.reply(`*Benar!*\n+${conn.siapakahaku[id][2]} XP`)
-            clearTimeout(conn.siapakahaku[id][3])
-            delete conn.siapakahaku[id]
-        } else if (m.text.toLowerCase().endsWith(json.result.jawaban.split` `[1])) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+            m.reply(`*Correct!*\n+${conn.who am I[id][2]} XP`)
+            clearTimeout(conn.who am I[id][3])
+            delete conn.who am I[en]
+        } else if (m.text.toLowerCase().endsWith(json.result.answer.split` `[1])) m.reply(`*A Little More!*`)
+        else m.reply(`*False!*`)
     }
     return !0
 }
