e27f487ac8524a9fc5b67ccd661fa82f30b13e77
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-26 17:12:05 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -44,7 +44,502 @@ const defaultMenu = {
   header: '╔══「 %category 」══❀',
   body: '  ║☆ %cmd %islimit %isPremium',
   footer: '╚════════════❀\n',
-  after: `
+  after: '
+‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎
+╭─「 Main 」
+〽️ /afk [alasan]
+〽️ /jadian (Limit)
+〽️ /menu
+〽️ /help
+〽️ /?
+╰────
+
+╭─「 Game 」
+〽️ /asahotak
+〽️ /caklontong
+〽️ /delsesittt (Limit)
+〽️ /family100
+〽️ /math <mode>
+〽️ /siapakahaku
+〽️ /suit [gunting|batu|kertas]
+〽️ /suitpvp @tag
+〽️ /suit2 @tag
+〽️ /tebakgambar
+〽️ /tebakkata
+〽️ /tebakkimia
+〽️ /tebaklagu (Limit)
+〽️ /tebaklirik
+〽️ /tictactoe [custom room name]
+〽️ /ttt [custom room name]
+╰────
+
+╭─「 Exp & Limit 」
+〽️ /buy<jumlah limit>
+〽️ /buy <jumlah limit>
+〽️ /buyall
+〽️ /daily
+〽️ /claim
+〽️ /leaderboard [jumlah user]
+〽️ /lb [jumlah user]
+〽️ /levelup
+〽️ /limit [@user]
+〽️ /pay @user <amount>
+〽️ /paylimit @user <amount>
+╰────
+
+╭─「 Sticker 」
+〽️ /attp <teks>
+〽️ /attp2 <teks> (Limit)
+〽️ /ctrigger <text> (Limit)
+〽️ /dadu
+〽️ /getexif
+〽️ /semoji [tipe] <emoji>
+〽️ /stiker (caption|reply media)
+〽️ /stiker <url>
+〽️ /stikergif (caption|reply media)
+〽️ /stikergif <url>
+〽️ /stikerline <url> (Limit)
+〽️ /stikertelegram <url> (Limit)
+〽️ /stikerly <pencarian> (Limit)
+〽️ /stickfilter (caption|reply media) (Limit)
+〽️ /stickmaker (caption|reply media) (Limit)
+〽️ /togif (reply)
+〽️ /toimg (reply)
+〽️ /toimg2 (reply)
+〽️ /tovideo (reply)
+〽️ /ttp <teks>
+〽️ /ttp2 <teks> (Limit)
+〽️ /ttpdark <teks> (Limit)
+〽️ /wm <packname>|<author>
+╰────
+
+╭─「 Kerang Ajaib 」
+〽️ apakah <teks>?
+〽️ /apakah <pertanyaan>
+〽️ /artinama <nama>
+〽️ kapan <text>?
+〽️ kapankah <text>?
+〽️ /kapan <pertanyaan>
+〽️ /kapankah <pertanyaan>
+╰────
+
+╭─「 Quotes 」
+〽️ /bucin
+〽️ /hacker
+〽️ /ilham
+╰────
+
+╭─「 Admin 」
+〽️ /add nomor,nomor (Limit)
+〽️ /+ nomor,nomor (Limit)
+〽️ /demote @user
+〽️ /member @user
+〽️ /↓ @user
+〽️ /kick @user (Limit)
+〽️ /- @user (Limit)
+〽️ /demote @user
+〽️ /member @user
+〽️ /↓ @user
+〽️ /promote @user
+〽️ /admin @user
+〽️ /^ @user
+〽️ /↑ @user
+╰────
+
+╭─「 Group 」
+〽️ /group *open / close*
+〽️ /enable <option>
+〽️ /disable <option>
+〽️ /getsider
+〽️ /infogrup
+〽️ /leavegc
+〽️ /leavegcall
+〽️ /leavegroup
+〽️ /linkgroup
+〽️ /here
+〽️ /listonline
+〽️ /opengumuman [teks]
+〽️ /oannounce [teks]
+〽️ /ohidetag [teks]
+〽️ /pengumuman [teks]
+〽️ /announce [teks]
+〽️ /hidetag [teks]
+〽️ /revoke
+〽️ /setpp
+〽️ /setbye <teks>
+〽️ /Setdesk <text>
+〽️ /Setname <text>
+〽️ /setwelcome <teks>
+〽️ /simulate <event> [@mention]
+〽️ /totalpesan
+〽️ /warn [@user]
+〽️ /Cekwarn @user
+〽️ /Delwarn @user
+╰────
+
+╭─「 Premium 」
+〽️ /join <chat.whatsapp.com>  (Premium)
+╰────
+
+╭─「 Internet 」
+〽️ /alkitab <pencarian>
+〽️ /anime <judul>
+〽️ /brainly <soal>
+〽️ /character <nama>
+〽️ /covid <negara>
+〽️ /darkjokes
+〽️ /fetch <url>
+〽️ /get <url>
+〽️ /gimage <query>
+〽️ /image <query>
+〽️ /google <pencarian>
+〽️ /googlef <pencarian>
+〽️ /epep <id>
+〽️ /katabijak <opsi>
+〽️ /kbbi <teks>
+〽️ /Layarkaca <query>
+〽️ /lirik <Apa>
+〽️ /manga <judul>
+〽️ /resep <makanan>
+〽️ /masak <makanan>
+〽️ /megumin
+〽️ /meme
+〽️ /neko
+〽️ pikachu
+〽️ /pinterest <keyword>
+〽️ /ppcouple
+〽️ /ppcp
+〽️ /loli
+〽️ /spotify <query>
+〽️ /ss <url>
+〽️ /ssf <url>
+〽️ /subreddit <query>
+〽️ /trendtwit
+〽️ /trendingtwitter
+〽️ /unsplash <keyword>
+〽️ /waifu
+〽️ /wallpaperanime (Limit)
+〽️ /wikipedia <apa>
+〽️ /Zodiakharian <zodiak>
+〽️ /Zodiakmingguan <zodiak>
+╰────
+
+╭─「 Anonymous Chat 」
+〽️ /start,leave,next
+╰────
+
+╭─「 MagerNulis & Logo 」
+〽️ /tahta <teks> (Limit)
+〽️ /magernulis1 <teks> (Limit)
+〽️ /magernulis2 <teks> (Limit)
+〽️ /magernulis3 <teks> (Limit)
+〽️ /magernulis4 <teks> (Limit)
+〽️ /magernulis5 <teks> (Limit)
+〽️ /magernulis6 <teks> (Limit)
+〽️ /nulis <teks>
+〽️ /quotemaker <teks>|<wm> (Limit)
+〽️ /quotemaker2 <teks | wm> (Limit)
+〽️ /tahta2<teks>
+╰────
+
+╭─「 Downloader 」
+〽️ /fb <url>
+〽️ /ig <url>
+〽️ /ighighlight <username>
+〽️ /igstalk <username>
+〽️ /igstory <username>
+〽️ /joox <judul> (Limit) (Premium)
+〽️ /play <pencarian> (Limit)
+〽️ /play2 <pencarian> (Limit)
+
+〽️ /tiktok <url>
+〽️ /twitter <url> (Limit)
+〽️ /ytmp3 <url> [server: id4, en60, en61, en68] (Limit)
+〽️ /yta <url> [server: id4, en60, en61, en68] (Limit)
+〽️ /ytmp4 <url> [server: id4, en60, en61, en68] (Limit)
+〽️ /ytv <url> [server: id4, en60, en61, en68] (Limit)
+〽️ /yt <url> [server: id4, en60, en61, en68] (Limit)
+╰────
+
+╭─「 Tools 」
+〽️ /aksara <opsi> <teks> (Limit)
+〽️ /asupan
+〽️ /base64
+〽️ /calc <expression>
+〽️ /carigrup <pencarian>
+〽️ /caripesan <pesan>|<jumlah>
+〽️ /hd (caption|reply media)
+〽️ /enhance (caption|reply media)
+〽️ /enphoto <effect> <text>|[text2]|[text3]
+〽️ /gimage <query>
+〽️ /image <query>
+〽️ /githubsearch <pencarian>
+〽️ /hadis
+〽️ /halah <teks>
+〽️ /hilih <teks>
+〽️ /huluh <teks>
+〽️ /heleh <teks>
+〽️ /holoh <teks>
+〽️ /tobraille
+〽️ /inspect <chat.whatsapp.com>
+〽️ /kodepos <kota> (Limit)
+〽️ /memeg<apa|apa>
+〽️ /mention <teks>
+〽️ /nulis2 <teks>
+〽️ /profile [@user]
+〽️ /qr <teks>
+〽️ /qrcode <teks>
+〽️ /readmore <teks>|<teks>
+〽️ /spoiler <teks>|<teks>
+〽️ /readviewonce
+〽️ /run (Limit)
+〽️ /scan [nomor]
+〽️ /spamcall <nomor> (Limit)
+〽️ /ping
+〽️ /speed
+〽️ /style <text>
+〽️ /textpro <effect> <text>|[text2]
+〽️ /translate <lang> <teks>
+〽️ /tts <lang> <teks>
+〽️ /upload (caption|reply media)
+〽️ /wait (caption|reply image)
+〽️ /wallpaperq (Limit)
+〽️ /yts <pencarian>
+〽️ /ytsearch <pencarian>
+〽️ /zodiac *2002 02 25*
+╰────
+
+╭─「 Fun 」
+〽️ /dadu
+〽️ /dare (Limit)
+〽️ /Namaninja <teks>
+〽️ /Purba <teks>
+〽️ /hug
+〽️ /pat
+〽️ /wink
+〽️ /jodoh <nama>|<nama doi> (Limit)
+〽️ /ref
+〽️ /simi <teks>
+〽️ /simsimi <teks>
+〽️ /simih <teks>
+〽️ /truth (Limit)
+╰────
+
+╭─「 Database 」
+〽️ /addvn <teks>
+〽️ /addmsg <teks>
+〽️ /addvideo <teks>
+〽️ /addgif <teks>
+〽️ /addaudio <teks>
+〽️ /addimg <teks>
+〽️ /addsticker <teks>
+〽️ /delcmd <text>
+〽️ /delvn <teks>
+〽️ /delmsg <teks>
+〽️ /delvideo <teks>
+〽️ /delgif <teks>
+〽️ /delaudio <teks>
+〽️ /delimg <teks>
+〽️ /delsticker <teks>
+〽️ /getvn <teks>
+〽️ /getmsg <teks>
+〽️ /getvideo <teks>
+〽️ /getgif <teks>
+〽️ /getaudio <teks>
+〽️ /getimg <teks>
+〽️ /getsticker <teks>
+〽️ /infocmd <text>
+〽️ /listcmd <text>
+〽️ /listall
+〽️ /listdoc
+〽️ /listvn
+〽️ /listmsg
+〽️ /listvideo
+〽️ /listgif
+〽️ /listaudio
+〽️ /listimg
+〽️ /liststicker
+〽️ /unlockcmd
+〽️ /lockcmd
+〽️ /unlockmsg
+〽️ /lockmsg
+〽️ /setcmd <text>
+╰────
+
+╭─「 Voting 」
+〽️ /cekvote
+〽️ /hapusvote
+〽️ /mulaivote [alasan]
+〽️ /upvote
+〽️ /devote
+╰────
+
+╭─「 Absen 」
+〽️ /cekabsen
+〽️ /hapusabsen
+〽️ /mulaiabsen [teks]
+〽️ /absen
+╰────
+
+╭─「 Al Qur'an 」
+〽️ /alquran <114> <1>
+〽️ /asmaulhusna [1-99]
+〽️ /hadis
+〽️ /salat <daerah>
+╰────
+
+╭─「 Jadi Bot 」
+〽️ /getcode
+〽️ /jadibot (Limit)
+〽️ /listjadibot
+〽️ /berhenti
+〽️ /stop
+╰────
+
+╭─「 Owner 」
+〽️ /whitelist nomor,nomor
+〽️ /addprem [@user]
+〽️ /banchat
+〽️ /ban
+〽️ /blocklist
+〽️ /broadcast <teks>
+〽️ /bc <teks>
+〽️ /broadcastgroup <teks>
+〽️ /bcgc <teks>
+〽️ /clearchat
+〽️ /clearchat chat
+〽️ /clearchat group
+〽️ /clearchat all
+〽️ /deletechat
+〽️ /deletechat chat
+〽️ /deletechat group
+〽️ /deletechat all
+〽️ /mutechat
+〽️ /mutechat chat
+〽️ /mutechat group
+〽️ /mutechat all
+〽️ /delprem [@user]
+〽️ /enable <option>
+〽️ /disable <option>
+〽️ /premlist
+〽️ /oadd @user
+〽️ /o+ @user
+〽️ /okick @user
+〽️ /o- @user
+〽️ /opromote @user
+〽️ /oadmin @user
+〽️ /o^ @user
+〽️ /setbotbio
+〽️ /setbotname
+〽️ /setbye <teks>
+〽️ /setexif <packname>|<owner>
+〽️ /setmenu <teks>
+〽️ /setmenubefore <teks>
+〽️ /setmenuheader <teks>
+〽️ /setmenubody <teks>
+〽️ /setmenufooter <teks>
+〽️ /setmenuafter <teks>
+〽️ /setwelcome <teks>
+〽️ /simulate <event> [@mention]
+〽️ /unbanchat
+〽️ /ban
+〽️ /upsw [text] (Reply Media)
+〽️ /upsw <text>
+╰────
+
+╭─「 Host 」
+〽️ /broadcastjadibot <teks>
+〽️ /bcbot <teks>
+〽️ /debounce
+〽️ /update
+〽️ /update2
+╰────
+
+╭─「 Advanced 」
+〽️ >
+〽️ =>
+╰────
+
+╭─「 Info 」
+〽️ /bannedlist
+〽️ /owner/creator
+〽️ /del
+〽️ /delete
+〽️ /donasi
+〽️ /groups
+〽️ /grouplist
+〽️ /bug <laporan>
+〽️ /report <laporan>
+〽️ /ping
+〽️ /speed
+╰────
+
+╭─「 No Category 」
+〽️ /save @mention <ContactName>
+╰────
+
+╭─「 videomaker 」
+〽️ /army <text>
+〽️ /bold <text>
+〽️ /colorful <text>
+〽️ /glowing <text>
+〽️ /poly <text>
+〽️ /retro <text>
+〽️ /shaunthesheep
+╰────
+
+╭─「 exp 」
+〽️ /ceksn
+〽️ /daftar <nama>.<umur>
+〽️ /reg <nama>.<umur>
+〽️ /register <nama>.<umur>
+〽️ /unreg <SN|SERIAL NUMBER>
+〽️ /unregister <SN|SERIAL NUMBER>
+╰────
+
+╭─「 maker 」
+〽️ /gay
+〽️ /hornycard
+〽️ /hornylicense
+〽️ /itssostupid
+〽️ /iss
+〽️ /stupid
+〽️ /lolice
+〽️ /renungan (Limit)
+〽️ /simpcard
+〽️ /trigger
+〽️ /ytcomment <comment>
+╰────
+
+╭─「 download 」
+〽️ /gitclone <url> (Limit)
+〽️ /githubdl (Limit)
+╰────
+
+╭─「 berita 」
+〽️ /detik
+〽️ /kompas
+〽️ /liputan6
+〽️ /tribun
+〽️ /jalantikus
+╰────
+
+╭─「 audio 」
+〽️ /tomp3 (reply)
+〽️ /tovn (reply)
+〽️ /bass [vn]
+〽️ /blown [vn]
+〽️ /deep [vn]
+〽️ /earrape [vn]
+〽️ /fast [vn]
+〽️ /fat [vn]
+〽️ /nightcore [vn]
+〽️ /reverse [vn]
+〽️ /robot [vn]
+〽️ /slow [vn]
+〽️ /smooth [vn]
+〽️ /tupai [vn]
+╰────'
 *%npmname@^%version*
 ${'```%npmdesc```'}
 `,
