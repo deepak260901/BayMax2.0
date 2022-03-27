8cb50c79843b34e74555a78c0ee3a1b1d16cbb5e
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 05:27:28 +0530

    Add files via upload

---
+++ plugins/Did.js
@@ -0,0 +1,15 @@
+let handler  = async (m, { conn }) => {
+let name = conn.getName(m.sender)
+let teks = `
+${pickRandom([` *Did you mean /help*`])}
+`.trim()
+conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [m.sender] }})
+}
+handler.customPrefix = /(^/|/i
+handler.command = new RegExp
+
+module.exports = handler
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)]
+}
