6dd7e9e51ac3553b69e5fd46d0cd58657f7caf39
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-27 14:46:46 +0530

    Update tahta2.js

--- plugins/tahta2.js
+++ plugins/tahta2.js
@@ -8,9 +8,9 @@ let handler  = async (m, { conn, text }) => {
   let tgl = d.toLocaleDateString('id-Id')
   let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
  text,
-await conn.sendFile(m.chat, global.API('xteam', '/tahta', { text, }, 'APIKEY'), 'Harta Tahta.png', 'Nih udah jadi Harta tahtamu...\n *_Tetap Support:_* *Drawl Nag*', m)
+await conn.sendFile(m.chat, global.API('xteam', '/tahta', { text, }, 'APIKEY'), 'Treasure Throne.png', 'Now, it's become your treasure...\n *_Keep Support:_* *Drawl Nag*', m)
 }
-handler.help = ['tahta2'].map(v => v + '<teks>')
+handler.help = ['throne2'].map(v => v + '<teks>')
 handler.tags = ['nulis']
 handler.command = /^tahta2$/i
 handler.owner = false
