33355dfbfbe601e017f5f5ce4a4706d90e48cf8d
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-27 14:30:42 +0530

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -19,7 +19,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
       auth = true
     }
     conn.on('qr', async qr => {
-      let scan = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk WhatsApp Web\n3. Scan QR ini \nQR Expired dalam 20 detik', m)
+      let scan = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, { scale: 8 }), 'qrcode.png', 'Scan this QR to become a temporary bot\n\n1. Click the three dots in the top right corner\n2. Tap WhatsApp Web\n3. Scan QR this \nQR Expires in 20 seconds', m)
       setTimeout(() => {
         parent.deleteMessage(m.chat, scan.key)
       }, 30000)
@@ -36,9 +36,9 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
     conn.on('group-participants-update', conn.onParticipantsUpdate)
     conn.regenerateQRIntervalMs = null
     conn.connect().then(async ({user}) => {
-      parent.reply(m.chat, 'Berhasil tersambung dengan WhatsApp - mu.\n*NOTE: Ini cuma numpang*\n' + JSON.stringify(user, null, 2), m)
+      parent.reply(m.chat, 'Successfully connected with WhatsApp - mu.\n*NOTE: It's just a ride*\n' + JSON.stringify(user, null, 2), m)
       if (auth) return
-      await parent.sendMessage(user.jid, `Kamu bisa login tanpa qr dengan pesan dibawah ini. untuk mendapatkan kode lengkapnya, silahkan kirim *${usedPrefix}getcode* untuk mendapatkan kode yang akurat`, MessageType.extendedText)
+      await parent.sendMessage(user.jid, `You can login without qr with the message below.  To get the complete code, please send *${usedPrefix}getcode* to get the accurate code`, MessageType.extendedText)
       parent.sendMessage(user.jid, `${usedPrefix + command} ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, MessageType.extendedText)
     })
     setTimeout(() => {
