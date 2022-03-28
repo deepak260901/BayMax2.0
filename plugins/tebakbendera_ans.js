988261335bb15eefb8cb4e6265826ff1d669f2b3
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 15:46:23 +0530

    Update tebakbendera_ans.js

--- plugins/tebakbendera_ans.js
+++ plugins/tebakbendera_ans.js
@@ -1,24 +1,24 @@
 const similarity = require('similarity')
-const threshold = 0.72
-let handler = m => m
-handler.before = async function (m) {
-    let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*benderaapa/i.test(m.quoted.text)) return !0
-    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
-    if (!(id in this.tebakbendera)) return m.reply('Soal itu telah berakhir')
-    if (m.quoted.id == this.tebakbendera[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
-        // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.DATABASE._data.users[m.sender].exp += this.tebakbendera[id][2]
-            m.reply(`*Benar!*\n+${this.tebakbendera[id][2]} XP`)
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
-        } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
-    }
-    return !0
-}
-handler.exp = 0
+ const threshold = 0.72
+ let handler = m => m
+ handler.before = async function (m) {
+     let id = m.chat
+     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*what flag/i.test(m.quoted.text)) return !0
+     this.guess the flag = this.guess the flag ?  this.guess the flag : {}
+     if (!(id in this.tebakflag)) return m.reply('The question has ended')
+     if (m.quoted.id == this.tebakflanda[id][0].id) {
+         let json = JSON.parse(JSON.stringify(this.tebakflag[id][1]))
+         // m.reply(JSON.stringify(json, null, '\t'))
+         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
+             global.DATABASE._data.users[m.sender].exp += this.tebakflag[id][2]
+             m.reply(`*Correct!*\n+${this.tebakflag[id][2]} XP`)
+             clearTimeout(this.tebakflag[id][3])
+             delete this.tebakflag[id]
+         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold) m.reply(`*A little more!*`)
+         else m.reply(`*False!*`)
+     }
+     returns !0
+ }
+ handler.exp = 0
 
-module.exports = handler
+ module.exports = handler
