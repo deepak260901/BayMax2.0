e7eb53321d8a889891b59747d07015c1f8992fe9
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 15:27:16 +0530

    Update tebakbendera.js

--- plugins/tebakbendera.js
+++ plugins/tebakbendera.js
@@ -1,34 +1,34 @@
 let fetch = require('node-fetch')
 
-let timeout = 120000
-let poin = 4999
-let src
-let handler = async (m, { conn, usedPrefix }) => {
-    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
-    let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
-        throw false
-    }
-    if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/BochilTeam/database/master/games/tebakbendera.json'))).json()
-    let json = src[Math.floor(Math.random() * src.length)]
-    if (!json) throw json
-    let caption = `
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}benderaapa untuk bantuan
-Bonus: ${poin} XP
-`.trim()
-    conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.img, 'img.jpg', caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakbendera[id][0])
-            delete conn.tebakbendera[id]
-        }, timeout)
-    ]
-}
-handler.help = ['tebakbendera']
-handler.tags = ['game']
-handler.command = /^tebakbendera/i
+ let timeout = 120000
+ let points = 4999
+ let src
+ let handler = async(m, { conn, usedPrefix }) => {
+     conn.guess the flag = conn.guess the flag ?  conn.guess the flag : {}
+     let id = m.chat
+     if (id in conn.tebakflag) {
+         conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.tebakflag[id][0])
+         throw false
+     }
+     if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/BochilTeam/database/master/games/tebakflag.json'))).json()
+     let json = src[Math.floor(Math.random() * src.length)]
+     if (!json) throw json
+     let caption = `
+ Timeout *${(timeout / 1000).toFixed(2)} seconds*
+ Type ${usedPrefix}what flag for help
+ Bonus: ${points} XP
+ `.trim()
+     conn.guessflag[id] = [
+         await conn.sendFile(m.chat, json.img, 'img.jpg', caption, m),
+         json, points,
+         setTimeout(() => {
+             if (conn.tebakflag[id]) conn.reply(m.chat, `Time is up!\nThe answer is *${json.name}*`, conn.tebakflag[id][0])
+             delete conn.tebakflag[id]
+         }, timeout)
+     ]
+ }
+ handler.help = ['guess the flag']
+ handler.tags = ['game']
+ handler.command = /^guess the flag/i
 
-module.exports = handler
+ module.exports = handler
