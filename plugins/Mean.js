597ad7a7973304d77a788d616355e8e1c992aa87
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 06:49:33 +0530

    Update Mean.js

--- plugins/Mean.js
+++ plugins/Mean.js
@@ -1,6 +1,6 @@
 module.exports = {
     before(m) {
-       if (/\b(bot)\b/i.test(m.text)) m.reply('Did you mean: ${usedPrefix}help ')
+       if (/\b(/)\b/i.test(m.text)) m.reply('Did you mean /help ')
        return !0
   }
 }
