a95850560bd5e30b73ad0867eb73d78794a09066
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 15:47:19 +0530

    Update tebakangka.js

--- plugins/tebakangka.js
+++ plugins/tebakangka.js
@@ -1,24 +1,24 @@
-let handler = async (m, { conn, args }) => {
-    const bonus = Math.floor(Math.random() * 300)
-    if (!args[0]) throw 'Harap masukan pilihan angka yang akan kamu pilih'
-    if (!/\d/i.test(args[0])) throw 'Pilih Angka 0 sampai 9 Yang Akan Kamu Pilih!'
-    const bot = pickRandom(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
-    const isWin = bot == args[0]
-    conn.reply(m.chat, `
-*「 TEBAK ANGKA 」*
-Angka Kamu : ${args[0]}
-Angka Bot : ${bot}
-Apakah Angkamu Sama Dengan Bot ?
-+${isWin ? bonus : 0} XP!
-`.trim(), m)
-    if (isWin) global.DATABASE.data.users[m.sender].exp += bonus * 1
-}
-handler.help = ['angka <0-9>']
-handler.tags = ['game']
-handler.command = /^angka/i
+let handler = async(m, { conn, args }) => {
+     const bonus = Math.floor(Math.random() * 300)
+     if (!args[0]) throw 'Please enter your preferred number'
+     if (!/\d/i.test(args[0])) throw 'Select the numbers 0 to 9 that you would like to choose!'
+     const bot = pickRandom(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+     const isWin = bot == args[0]
+     conn.reply(m.chat, `
+ *「 GUESS THE NUMBERS *
+ Your Number : ${args[0]}
+ Bot Number : ${bot}
+ Are your numbers the same as bots?
+ +${isWin ?  bonuses : 0} XP!
+ `.trim(), m)
+     if (isWin) global.DATABASE.data.users[m.sender].exp += bonus * 1
+ }
+ handler.help = ['number <0-9>']
+ handler.tags = ['game']
+ handler.command = /^number/i
 
-module.exports = handler
+ module.exports = handler
 
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)]
-}
+ function pickRandom(list) {
+     return list[Math.floor(Math.random() * list.length)]
+ }
