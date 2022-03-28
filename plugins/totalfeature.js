cb33711b2e540921054d61057fd1beb8abef7d09
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 15:26:10 +0530

    Update totalfeature.js

--- plugins/totalfeature.js
+++ plugins/totalfeature.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 let totalfeatures = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-conn.sendButton(m.chat, `_*total features: ${totalfeatures}*_`, '©games-wabot', 'Menu', '.menu', m) 
+conn.sendButton(m.chat, `_*total features: ${totalfeatures}*_`, '©ZeroTwo 2022', 'Menu', '/menu', m) 
 }
 
 handler.help = ['totalfeature']
