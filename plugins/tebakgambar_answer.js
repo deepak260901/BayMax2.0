b9e02855ab3e4379f08152770524522db6b7682a
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 15:23:11 +0530

    Update tebakgambar_answer.js

--- plugins/tebakgambar_answer.js
+++ plugins/tebakgambar_answer.js
@@ -3,19 +3,19 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
   let id = m.chat
-  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*hint/i.test(m.quoted.text)) return !0
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*hint/i.test(m.quoted.text)) return !0
   this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
-  if (!(id in this.tebakgambar)) return m.reply('Soal itu telah berakhir')
+  if (!(id in this.tebakgambar)) return m.reply('The matter has ended')
   if (m.quoted.id == this.tebakgambar[id][0].id) {
     let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
     // m.reply(JSON.stringify(json, null, '\t'))
     if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
       global.DATABASE._data.users[m.sender].exp += this.tebakgambar[id][2]
-      m.reply(`*Benar!*\n+${this.tebakgambar[id][2]} XP`)
+      m.reply(`*Right!*\n+${this.tebakgambar[id][2]} XP`)
       clearTimeout(this.tebakgambar[id][3])
       delete this.tebakgambar[id]
-    } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-    else m.reply(`*Salah!*`)
+    } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*a little more!*`)
+    else m.reply(`*Wrong!*`)
   }
   return !0
 }
