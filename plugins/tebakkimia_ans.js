5b792fc7c00aefad783b88046acd21cde55d8516
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 14:44:11 +0530

    Update tebakkimia_ans.js

--- plugins/tebakkimia_ans.js
+++ plugins/tebakkimia_ans.js
@@ -3,19 +3,19 @@ const threshold = 0.72
 module.exports = {
     async before(m) {
         let id = m.chat
-        if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teki/i.test(m.quoted.text)) return !0
+        if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*riddle/i.test(m.quoted.text)) return !0
         this.tebakkimia = this.tebakkimia ? this.tebakkimia : {}
-        if (!(id in this.tebakkimia)) return m.reply('Soal itu telah berakhir')
+        if (!(id in this.tebakkimia)) return m.reply('The matter has ended')
         if (m.quoted.id == this.tebakkimia[id][0].id) {
-            let json = JSON.parse(JSON.stringify(this.tebakkimia[id][1]))
+            let json = JSON.parse(JSON.stringify(this.guess chemistry[id][1]))
             // m.reply(JSON.stringify(json, null, '\t'))
             if (m.text.toLowerCase() == json.unsur.toLowerCase().trim()) {
-                global.DATABASE._data.users[m.sender].exp += this.tebakkimia[id][2]
-                m.reply(`*Benar!*\n+${this.tebakkimia[id][2]} XP`)
-                clearTimeout(this.tebakkimia[id][3])
-                delete this.tebakkimia[id]
-            } else if (similarity(m.text.toLowerCase(), json.unsur.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-            else m.reply(`*Salah!*`)
+                global.DATABASE._data.users[m.sender].exp += this.guess chemistry[id][2]
+                m.reply(`*Right!*\n+${this.guess chemistry[id][2]} XP`)
+                clearTimeout(this.guess chemistry[id][3])
+                delete this.guess chemistry[id]
+            } else if (similarity(m.text.toLowerCase(), json.unsur.toLowerCase().trim()) >= threshold) m.reply(`*a little more!*`)
+            else m.reply(`*Wrong!*`)
         }
         return !0
     },
