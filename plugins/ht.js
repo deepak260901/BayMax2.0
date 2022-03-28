efbdee3de0df5ee4979cba33d4ca415026a026b1
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 15:10:22 +0530

    Update ht.js

--- plugins/ht.js
+++ plugins/ht.js
@@ -1,12 +1,12 @@
 let fs = require('fs')
 let handler = async (m, { conn, text }) => {
   conn.hartatahta = conn.hartatahta ? conn.hartatahta : {}
-  if (m.chat in conn.hartatahta) throw 'Masih ada yang sedang membuat\nTeks Harta Tahta\ndi chat ini... tunggu sampai selesai'
+  if (m.chat in conn.hartatahta) throw 'There are still those who are making\Treasure of Thrones\text in this chat...wait for it to finish'
   else conn.hartatahta[m.chat] = true
-  m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
+  m.reply('_is making..._\n*Please wait about 1 minute*')
   try {
     let img = await ht(text ? text : ':v')
-    conn.sendFile(m.chat, img, 'Harta Tahta.png', '*© Nurutomo*\nMade with FFmpeg', m)
+    conn.sendFile(m.chat, img, 'Treasure Tahta.png', '*© Toshiro*\nMade with FFmpeg', m)
     setTimeout(() => {
       fs.unlinkSync(img)
     }, 5000);
