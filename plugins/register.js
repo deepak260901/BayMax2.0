5557fd2a46ba521d6023f90052bacc81288c53b4
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 15:41:39 +0530

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -6,20 +6,21 @@ let handler = async function (m, { text, usedPrefix }) {
   if (!Reg.test(text)) throw `Incorrect format\n*${usedPrefix}list <name>.<age>*`
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Name cannot be empty (Alphanumeric)'
-  if (!age) throw 'Age can't be empty (Number)'
+  if (!age) throw 'Age cant be empty (Number)'
   user.name = name
   user.age = parseInt(age)
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-Daftar berhasil!
+Register successfully!
 
-╭─「 Info 」
-│ Name: ${name}
-│ Age: ${age}thn
-│ SN: ${sn}
-╰────
+┌──❁┈[ REGISTRATION ]┈❁───
+├➸ Successfully registered In ZeroTwo
+├➸ *Name*: ${name}~
+├➸ *Age*: ${age}
+├➸ *Sn*: ${sn}
+└─────❁┈[ Zero Two ]┈❁─────
 `.trim())
 }
 handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <name>.<age>')
