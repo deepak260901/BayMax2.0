6c861be7fc4edbf4b6220285a4eaca52af2fe1d7
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-29 19:19:59 +0530

    Update suitpvp.js

--- plugins/suitpvp.js
+++ plugins/suitpvp.js
@@ -7,18 +7,18 @@ let poin = 1000
 let poin_lose = 500
 let handler = async (m, { conn, usedPrefix }) => {
   conn.suit = conn.suit ? conn.suit : {}
-  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw 'Selesaikan suit mu yang sebelumnya'
-  if (!m.mentionedJid[0]) return m.reply(`_Siapa yang ingin kamu tantang?_\nTag orangnya.. Contoh\n\n${usedPrefix}suit @${owner[0]}`, m.chat, { contextInfo: { mentionedJid: [owner[0] + '@s.whatsapp.net'] } })
-  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `Orang yang kamu tantang sedang bermain suit bersama orang lain :(`
+  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.sender))) throw 'Complete your previous suit'
+  if (!m.mentionedJid[0]) return m.reply(`_Who do you want to challenge?_\nTag the person.. Example\n\n${usedPrefix}suit @${owner[0]}`, m.chat, { contextInfo: { mentionedJid: [owner[0] + '@s.whatsapp.net'] } })
+  if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `The person you are challenging is playing suit with someone else :(`
   let id = 'suit_' + new Date() * 1
   let caption = `
       *「 SUIT PVP 」*
 
-@${m.sender.split`@`[0]} menantang @${m.mentionedJid[0].split`@`[0]} untuk bermain suit
+@${m.sender.split`@`[0]} challenging @${m.mentionedJid[0].split`@`[0]} to play suit
 
-Silahkan @${m.mentionedJid[0].split`@`[0]}
+Please @${m.mentionedJid[0].split`@`[0]}
 
-Ketik "terima/ok/gas" untuk memulai suit\nKetik "tolak/gabisa/nanti" untuk menolak
+Type "accept/ok/gas" to start suit\nKetik "refuse/cannot/later" to refuse
 `.trim()
   conn.suit[id] = {
     chat: await conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } }),
@@ -27,7 +27,7 @@ Ketik "terima/ok/gas" untuk memulai suit\nKetik "tolak/gabisa/nanti" untuk menol
     p2: m.mentionedJid[0],
     status: 'wait',
     waktu: setTimeout(() => {
-      if (conn.suit[id]) conn.reply(m.chat, `_Waktu suit habis_`, m)
+      if (conn.suit[id]) conn.reply(m.chat, `_Suit time out_`, m)
       delete conn.suit[id]
     }, timeout), poin, poin_lose, timeout
   }
