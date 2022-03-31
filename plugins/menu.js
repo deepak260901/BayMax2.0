6b97ebbb4f05644f1283f6ac24b760d81955d580
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-31 11:36:23 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -24,34 +24,31 @@ let handler  = async (m, { conn, usedPrefix: _p, DevMode }) => {
     let uptime = clockString(_uptime)
     let totalreg = Object.keys(global.DATABASE._data.users).length
     let tags = {
-      'main': 'Main',
-      'game' : 'Games',
-      'maker' : 'Maker',
-      'about': 'About And Info',
-      'rpg': 'Rpg',
-      'xp': 'Exp & Limit',
-      'premium': 'Premium',
-      'image' : 'Image',
-      'indonesian' : 'For Indonesians',
-      'game': 'Games',
-      'sticker': 'Sticker',
-      'spammer' : 'Spammer',
-      'audio': 'audio',
-      'database': 'Database',
-      'quotes': 'Quotes',
-      'internet': 'Internet',
-      'downloader': 'Downloader',
-      'tools': 'Tools',
-      'admin': 'Admin',
-	  'ep' : 'ep',
-	  'te' : 'te',
-      'group': 'Group',
-      'owner': 'Owner',
-      'host': 'Host',
-      'tools': 'tool',
-      'advanced': 'Advanced',
-      'info': 'Info',
-      '': 'No Category',
+       'main': 'Main',
+  'rpg': 'Epic RPG',
+  'game': 'Game',
+  'xp': 'Exp & Limit',
+  'sticker': 'Sticker',
+  'shell': 'Magic Shell',
+  'admin': 'Admin',
+  'group': 'Group',
+  'premium': 'Premium',
+  'internet': 'Internet',
+  'anonymous': 'Anonymous Chat',
+  'nulis': 'MagerNulis & Logo',
+  'downloader': 'Downloader',
+  'tools': 'Tools',
+  'fun': 'Fun',
+  'database': 'Database',
+  'vote': 'Voting',
+  'nsfw': 'Nsfw',
+  'absen': 'Absen',
+  'jadibot': 'Jadi Bot',
+  'owner': 'Owner',
+  'host': 'Host',
+  'advanced': 'Advanced',
+  'info': 'Info',
+  '': 'No Category',
     }
     for (let plugin of Object.values(global.plugins))
       if (plugin && 'tags' in plugin)
@@ -74,19 +71,20 @@ let handler  = async (m, { conn, usedPrefix: _p, DevMode }) => {
     }
     conn.menu = conn.menu ? conn.menu : {}
     let before = conn.menu.before || ` 
-(◍•ᴗ•◍)𝗞𝗼𝗻𝗻𝗶𝗰𝗵𝗶𝘄𝗮 %name 𝗗𝗮𝗿𝗹𝗶𝗻𝗴 👋🏻!
-𝗡𝗼𝘁𝗲📝: 𝗜𝗳 𝗬𝗼𝘂 𝗛𝗮𝘃𝗶𝗻𝗴 𝗔𝗻𝘆 𝗧𝘆𝗽𝗲 𝗢𝗳 𝗜𝘀𝘀𝘂𝗲 𝗧𝗵𝗲𝗻 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 𝗢𝘄𝗻𝗲𝗿 𝗢𝗿 𝗧𝘆𝗽𝗲 /𝗿𝗲𝗽𝗼𝗿𝘁 𝗙𝗼𝗿 𝘆𝗼𝘂𝗿 𝗜𝘀𝘀𝘂𝗲!
-┏ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━
-┃ *𝗕𝗼𝘁 𝗡𝗮𝗺𝗲* : ${conn.user.name}
-┃ *𝗚𝗿𝗼𝘂𝗽𝘀 𝗖𝗵𝗮𝘁𝘀* : ${conn.chats.array.filter(v => v.jid.endsWith('g.us')).map(v => v.jid).length}
-┃ *𝗣𝗲𝗿𝘀𝗻𝗼𝗮𝗹 𝗖𝗵𝗮𝘁𝘀* : ${conn.chats.array.filter(v => v.jid.endsWith('s.whatsapp.net')).map(v => v.jid).length}
-┃ *𝗨𝗽𝘁𝗶𝗺𝗲 𝗕𝗼𝘁* : ${clockString(process.uptime() * 1000)}
-┃ *𝗛𝗼𝘀𝘁 𝗡𝘂𝗺𝗯𝗲𝗿* : @${global.conn.user.jid.split('@')[0]}
-┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━ %readmore`.trimStart()
-let header = conn.menu.header || '┏━「 %category 」━━'
+
+(❤️◍•ᴗ•◍❤️)Konnichiwa %name Darling 👋🏻
+Note📝: If You Having Any Type Of Issues And Bug Then Contact Owner Or Type /Report And Describe About Issue!
+┏ ┅ ━━━━━━━━━━━━━━━ ┅ ━❀
+┃ 𝖡𝗈𝗍 𝖭𝖺𝗆𝖾 : ${conn.user.name}
+┃ 𝖦𝗋𝗈𝗎𝗉𝗌 𝖢𝗁𝖺𝗍𝗌 : ${conn.chats.array.filter(v => v.jid.endsWith('g.us')).map(v => v.jid).length}
+┃ 𝖯𝗋𝖾𝗌𝗇𝗈𝖺𝗅 𝖢𝗁𝖺𝗍𝗌 : ${conn.chats.array.filter(v => v.jid.endsWith('s.whatsapp.net')).map(v => v.jid).length}
+┃ 𝖴𝗉𝗍𝗂𝗆𝖾 𝖡𝗈𝗍 : ${clockString(process.uptime() * 1000)}
+┃ 𝖧𝗈𝗌𝗍 𝖭𝗎𝗆𝖻𝖾𝗋 : @${global.conn.user.jid.split('@')[0]}
+┗ ┅ ━━━━━━━━━━━━━━━ ┅ ━❀ %readmore`.trimStart()
+let header = conn.menu.header || '┏━「 %category 」━━❀'
     let body   = conn.menu.body   || '┃ 〽️ %cmd%islimit'
-    let footer = conn.menu.footer || '┗ ┅ ━━━━━━━━━━━ ┅ ━\n\n'
-    let after  = conn.menu.after  || `\nゼロツー❤️\n``
+    let footer = conn.menu.footer || '┗ ┅ ━━━━━━━━ ┅ ━❀\n\n'
+    let after  = conn.menu.after  || `\nゼロツー❤️\n'
     let _text  = before + '\n'
     for (let tag in groups) {
       _text += header.replace(/%category/g, tags[tag]) + '\n'
@@ -109,7 +107,9 @@ let header = conn.menu.header || '┏━「 %category 」━━'
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => replace[name])
-    conn.sendFile(m.chat, fs.readFileSync(`./src/pew.mp4`), 'pew.mp4', text.trim(), m)
+    let pp = await conn.getProfilePicture(conn.user.jid).catch(_ => path.join(__dirname, '../src/avatar_contact.png'))
+    conn.sendButton(m.chat,text.trim(), author,  pp,  [
+], { quoted: m}).catch(_ => conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m)).catch(_ => conn.reply(m.chat, text.trim(), m))
   } catch (e) {
     conn.reply(m.chat, 'Sorry Menu Error!!r', m)
     throw e
