cda539948361ec88664cb96ea92ed0f3accf35c8
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-31 11:47:59 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -109,7 +109,9 @@ let header = conn.menu.header || '╔═「 %category 」══❀'
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).join`|`})`, 'g'), (_, name) => replace[name])
-    conn.sendFile(m.chat, fs.readFileSync(`./src/pew.mp4`), 'pew.mp4', text.trim(), m)
+     let pp = await conn.getProfilePicture(conn.user.jid).catch(_ => path.join(__dirname, '../src/avatar_contact.png'))
+    conn.sendButton(m.chat,text.trim(), author,  pp,  [
+], { quoted: m}).catch(_ => conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m)).catch(_ => conn.reply(m.chat, text.trim(), m))
   } catch (e) {
     conn.reply(m.chat, 'Sorry Menu Error!!r', m)
     throw e
