be739a31fbbff28f22fa0045a0cad0fa86d81766
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-04-01 10:04:25 +0530

    Update vote-vote.js

--- plugins/vote-vote.js
+++ plugins/vote-vote.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) throw `_*tidak ada voting digrup ini!*_\n\n*${usedPrefix}mulaivote* - untuk memulai vote`
+    if (!(id in conn.vote)) throw `_*no voting in this group!*_\n\n*${usedPrefix}mulaivote* - to start voting`
     let isVote = conn.vote[id][1].concat(conn.vote[id][2])
     const wasVote = isVote.includes(m.sender)
-    if (wasVote) throw 'Kamu sudah vote!'
+    if (wasVote) throw 'You have voted!'
     if (/up/i.test(command)) {
         conn.vote[id][1].push(m.sender)
     } else if (/de/i.test(command)) {
         conn.vote[id][2].push(m.sender)
     }
-    m.reply(`Done!\n\n*${usedPrefix}cekvote* - untuk mengecek vote`)
+    m.reply(`Done!\n\n*${usedPrefix}cekvote* - to check the vote`)
 
 }
 handler.help = ['vote']
