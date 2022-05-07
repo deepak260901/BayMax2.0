2f228bed4ac9f86cabdff07c976fb619655944dd
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-05-07 09:50:37 +0530

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -1,11 +1,11 @@
 let handler  = async (m, { conn }) => {
 let name = conn.getName(m.sender)
 let teks = `
-${pickRandom([` *Hello👋 @${m.sender.split`@`[0]}_\n\n_Darling How are you😊*`])}
+${pickRandom([` *Hello👋 @${m.sender.split`@`[0]}*`])}
 `.trim()
 conn.reply(m.chat, teks, m, { contextInfo: { mentionedJid: [m.sender] }})
 }
-handler.customPrefix = /Hello|Heya|hlo/i
+handler.customPrefix = /Hello|Heya|hlo/hi/hii/i
 handler.command = new RegExp
 
 module.exports = handler
