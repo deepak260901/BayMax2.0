6fddd46ead00737b6da101f42e2b8d916f402d66
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-05-07 17:33:20 +0530

    Update throne2.js

--- plugins/throne2.js
+++ plugins/throne2.js
@@ -8,7 +8,7 @@ let handler  = async (m, { conn, text }) => {
   let tgl = d.toLocaleDateString('id-Id')
   let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
  text,
-await conn.sendFile(m.chat, global.API('xteam', '/tahta', { text, }, 'APIKEY'), 'Treasure Throne.png', 'Now, it's become your treasure...\n *_Keep Support:_* *Drawl Nag*', m)
+await conn.sendFile(m.chat, global.API('xteam', '/tahta', { text, }, 'APIKEY'), 'Treasure Throne.png', 'Now, its become your treasure...\n *_Keep Support:_* *Drawl Nag*', m)
 }
 handler.help = ['throne2'].map(v => v + '<teks>')
 handler.tags = ['nulis']
