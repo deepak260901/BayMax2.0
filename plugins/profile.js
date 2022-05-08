244d36cd5eff43fdbe17b30c9897488b68227595
Toshiro <91844428+Toshiro223@users.noreply.github.com>
2022-05-08 14:58:23 +0530

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,30 +1,33 @@
-let handler = async (m, { conn, usedPrefix }) => {
+let PhoneNumber = require('awesome-phonenumber')
+let handler = async (m, { conn }) => {
   let pp = './src/avatar_contact.png'
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
-    pp = await conn.getProfilePicture(m.sender)
+    pp = await conn.getProfilePicture(who)
   } catch (e) {
 
   } finally {
-    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
+    let { name, premium, level, limit, exp, lastclaim, registered, regTime, age } = global.db.data.users[m.sender]
+    let username = conn.getName(who)
     let str = `
-┌──────────────────❀
-│  [ User Profile ]
-├─────────────────❀
-│➸ Name: ${name.vnmae || name.notify || name.name || ('+' + name.jid.split`@`[0])} (@${m.sender.replace(/@.+/, '')})
-│➸ Number: +${m.sender.split`@`[0]}
-│➸ Link: https://wa.me/${m.sender.split`@`[0]}
-├─────────────────❀
-${readMore}
-\n\n*Want to check the contents of your inventory?  Type ${usedPrefix}inv*
+
+┏━━°❀❬ *PROFILE* ❭❀°━━┓
+┃ • *Name:* ${username} ${registered ? '(' + name + ') ': ''}
+┃ • *Tag:* @${who.replace(/@.+/, '')}
+┃ • *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+┃ • *Link:* https://wa.me/${who.split`@`[0]}
+┃ • ${registered ? '🎨 • *Age:* ' + age : ''}
+┃ •${about ? '🗒️ • *About:* ' + about : ''}
+┃ • *Premium:* ${premium ? "✅" :"❌"}
+┃ • *Registered:* ${registered ? '✅': '❌'}
+┗━━━━━━━━━━━━━━━━
 `.trim()
-    let mentionedJid = [m.sender]
+    let mentionedJid = [who]
     conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
   }
 }
-handler.help = ['profile']
-handler.tags = ['rpg']
-handler.command = /^(profile|profil|pp|propile|propil)$/i
+handler.help = ['profile [@user]']
+handler.tags = ['tools']
+handler.command = /^profile|pp$/i
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
